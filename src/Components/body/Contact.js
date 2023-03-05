1ff2226db19b207be6ff8e7a1f02cc1b12d1bfa5
majedulleon <majedulleon@gmail.com>
2023-03-05 11:40:08 +0600

    Minor fix

--- src/Components/body/Contact.js
+++ src/Components/body/Contact.js
@@ -14,7 +14,8 @@ const mapDispatchToProps = dispatch => {
 
 const required = (val) => val && val.length;
 const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[A - Z0 -9._ % +-] +@[A - Z0 - 9. -]+\.[A - Z]{ 2, 4 }$/i.test(val);
+const validEmail = (val) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val);
+
 
 class Contact extends Component {
 
